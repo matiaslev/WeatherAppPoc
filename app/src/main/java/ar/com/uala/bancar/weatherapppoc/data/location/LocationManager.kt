c28300e2438612f8690f6973df5119d714c544ad
matias lev <matias.lev@uala.com.ar>
2021-03-14 01:08:47 -0300

    location null cases TODO added

--- app/src/main/java/ar/com/uala/bancar/weatherapppoc/data/location/LocationManager.kt
+++ app/src/main/java/ar/com/uala/bancar/weatherapppoc/data/location/LocationManager.kt
@@ -5,13 +5,19 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Looper
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 class LocationManager(
     private val focusedLocationProviderClient: FusedLocationProviderClient,
     private val context: Context
 ) {
+
+    private lateinit var locationCallback: LocationCallback
 
     fun hasCoarseLocationPermission(): Boolean = ActivityCompat
         .checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) !=
@@ -19,9 +25,33 @@ class LocationManager(
 
     @SuppressLint("MissingPermission") // Contemplated in hasCoarseLocationPermission
     fun getLastKnownLocation(action: (Location?) -> Unit) {
-        focusedLocationProviderClient.lastLocation.addOnSuccessListener {
-            action(it)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                action(locationResult.lastLocation)
+                focusedLocationProviderClient.removeLocationUpdates(locationCallback)
+            }
         }
+
+        focusedLocationProviderClient.lastLocation.addOnSuccessListener {
+            if (it != null) {
+                action(it)
+            } else {
+                /** TODO: In some cases the Location Could be Null, we could listen location updates
+                 * Documentation: https://developer.android.com/training/location/retrieve-current
+                 * */
+
+                // startLocationUpdates(locationCallback)
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission") // Contemplated in getLastKnownLocation process
+    fun startLocationUpdates(locationCallback: LocationCallback) {
+        focusedLocationProviderClient.requestLocationUpdates(
+            LocationRequest.create(),
+            locationCallback,
+            Looper.getMainLooper()
+        )
     }
 
 }
